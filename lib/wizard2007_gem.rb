1a6b255f91c500869d5616a2491455fbd97cb0df
wizard2007 <sponsor85@mail.ru>
2016-10-24 23:55:00 +0300

    work gem with module

--- lib/wizard2007_gem.rb
+++ lib/wizard2007_gem.rb
@@ -1,9 +1,7 @@
 require "wizard2007_gem/version"
 
 module Wizard2007Gem
-  class Wizard2007_gem
-  	def self.hello
-  		'Hello, cruel world!'
-  	end
+  def self.hello
+      'Hello, cruel world!'  
   end
 end
